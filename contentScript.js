9b722225ae6dbedb58a3cba035000c6140fc92ee
Roshan <roshansalian1997@gmail.com>
2023-03-07 23:11:55 +0530

    Basic Functionality completed

--- contentScript.js
+++ contentScript.js
@@ -2,6 +2,8 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     const observer = new MutationObserver((mutationsList) => {
         let oneTime = false;
         for(const mutation of mutationsList){
+            // data-test-modal is a div that covers the entire div in the 'Add a note'  div
+            // need to add one more div with the 'where u know' modal, has the same data-test-modal
             if (oneTime == false && mutation.target.getAttribute('data-test-modal')===''){
                 console.log("About to click Send button");
                 clickSendButton();
@@ -11,16 +13,71 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         }
     });
 
-    function clickSendButton() {
-        const sendButton = document.querySelector('.artdeco-modal__actionbar .artdeco-button--primary');
+    function addNoteModal(){
+        console.log("In the note function, Regular");
+
+        //this is only to display the name
         const name = document.querySelector('span.flex-1');
         const name2 = name.querySelector('strong').textContent;
-        // print the Name
         console.log("Name: ", name2);
+
+        const sendButton = document.querySelector('.artdeco-modal__actionbar .artdeco-button--primary');
+
         if (sendButton) {
             sendButton.click();
-            //clicks successfully but messesup when searchign the next 'Connect' button
         }
+    }
+
+    function clickSendButton() {
+        const addANote = document.querySelector('button[aria-label="Add a note"]');
+
+        
+
+        //Collecting the tag for 'How u know?' modal
+        // write logic to check in below button is null or not null and based on that make logic 
+        const workColeagues = document.querySelector('button[aria-label="Work Colleagues"]');
+
+        // if(addANote){
+        //     addNoteModal();
+        // }
+        
+        if(workColeagues){
+            console.log("In the 'How u know' section, hard One");
+
+            // add the same school button
+            const workEvent = document.querySelector('button[aria-label="Met at a work-related event"]');
+
+            const howYouKnow = document.querySelector('button[aria-label="Connect"]');
+
+
+            let firstClick = false;
+
+            function firstClickHandler(){
+                workColeagues.click();
+                console.log("clicked work Coleague");
+                firstClick = true;
+            }
+
+            function secondClickHandler(){
+                if(firstClick){
+                    setTimeout(()=>{
+                        howYouKnow.click();
+                        console.log("Clicked send connection");
+                    }, 1000);
+                }
+                else{
+                    console.log("Reached else, some issue");
+                }
+            }
+
+            firstClickHandler();
+            secondClickHandler();
+            // Need to handle the Send note section again here. 
+            // Work coleague selection is successful here
+        }
+
+        addNoteModal();
+
     }
 
     if(message.type == 'start'){
